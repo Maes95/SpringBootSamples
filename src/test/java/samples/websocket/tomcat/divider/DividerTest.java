e20b61b3d27954afabfc6a3675176adcdb47cfb6
Maes95 <michel.maes95@gmail.com>
2018-10-17 15:42:57 +0200

    Add more Test

--- src/test/java/samples/websocket/tomcat/divider/DividerTest.java
+++ src/test/java/samples/websocket/tomcat/divider/DividerTest.java
@@ -13,4 +13,18 @@ public class DividerTest {
         assertEquals(result, "Result is: 5");
     }
 
+    @Test
+    public void divide2() {
+        Divider d = new Divider();
+        String result = d.divide("20 5");
+        assertEquals(result, "Result is: 4");
+    }
+
+    @Test
+    public void divide3() {
+        Divider d = new Divider();
+        String result = d.divide("20 2");
+        assertEquals(result, "Result is: 10");
+    }
+
 }
